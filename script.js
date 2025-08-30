d39cb15a770304054f378b1bdb14311c8902219c
cyanryann <209932180+cyanryann@users.noreply.github.com>
2025-08-30 19:27:06 -0400

    set grid and button to reset

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
 const grid = document.querySelector(".grid");
-function makeGrid() {
-    for (let i = 0; i < 16; i++)
+const gridmaker = document.querySelector("#gridmaker");
+function makeGrid(x = 20) {
+    for (let i = 0; i < x; i++)
     {
         let row = document.createElement("div");
         row.style.display = "flex";
         row.style.flexDirection = "row";
+        row.style.justifyContent = "space-between";
+        row.style.flexGrow = 1
         grid.append(row);
-        for (let k = 0; k < 16; k++)
+        for (let k = 0; k < x; k++)
         {
             let cell = document.createElement("div");
+            cell.style.flexGrow=1;
             cell.id = "cell";
             row.append(cell);
         }
     }
+    console.log("building");
 }
 makeGrid();
+function clearGrid() {
+    while (grid.childElementCount != 0)
+    {
+        grid.removeChild(grid.firstElementChild);
+        console.log(grid.childElementCount);
+    }
+    console.log("clearing");
+
+}
+gridmaker.addEventListener("click", () => {
+    clearGrid();
+    let x = prompt("How many rows and columns do you want!");
+    x = Math.min(100, x);
+    makeGrid(x);
+})
